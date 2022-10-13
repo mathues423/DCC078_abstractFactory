09295be8cce6a45fcab3e3c14cefe60bc6e73453
Matheus dos Reis Casarim <matheuscasarim22@hotmail.com>
2022-10-13 13:38:38 -0300

    Adição de teste

--- abstractFactory/src/test/java/FuncionarioTest.java
+++ abstractFactory/src/test/java/FuncionarioTest.java
@@ -3,22 +3,30 @@ import org.junit.jupiter.api.Test;
 
 class FuncionarioTest {
     @Test
-    public void deveTestarHistoricoVendedor(){
-
+    public void deveTestarHorarioVendedor(){
+        FabricaUsuario fabrica = new FabricaVendedor();
+        Funcionario vendedor = new Funcionario(fabrica);
+        assertEquals("Historico do Vendedor",vendedor.emitirHorario());
     }
 
     @Test
-    public void deveTestarHistoricoGerente(){
-
+    public void deveTestarHorarioGerente(){
+        FabricaUsuario fabrica = new FabricaGerente();
+        Funcionario vendedor = new Funcionario(fabrica);
+        assertEquals("Historico do Gerente",vendedor.emitirHorario());
     }
 
     @Test
     public void deveTestarVendasVendedor(){
-
+        FabricaUsuario fabrica = new FabricaVendedor();
+        Funcionario vendedor = new Funcionario(fabrica);
+        assertEquals("Vendas do Vendedor",vendedor.emitirRelatorioVenda());
     }
 
     @Test
     public void deveTestarVendasGerente(){
-
+        FabricaUsuario fabrica = new FabricaGerente();
+        Funcionario vendedor = new Funcionario(fabrica);
+        assertEquals("Vendas do Gerente",vendedor.emitirRelatorioVenda());
     }
 }
